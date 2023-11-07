75d4569c51707f7a30f084a5e03aa1d3ae75377f
NayoBd <yoann.barbrel@gmail.com>
2023-11-07 13:14:18 +0100

    uno

--- play/cards.py
+++ play/cards.py
@@ -1,7 +1,30 @@
-class Setup():
-    def __init__(self, couleur, valeur):
-        self.couleur = couleur
-        self.valeur = valeur
+import random
 
-    def __str__(self):
-        return f"{self.couleur} {self.valeur}"
+def builddeck():
+    """
+    Generate the Uno deck of 121 cards
+    Parametres : None
+    Return values : deck->list
+    """
+    deck = []
+    colours = ["Red","Green","Blue","Yellow","White"]
+    values = [0,1,2,3,4,5,6,7,8,9,"+2","skip","Reverse"]
+    for colour in colours:
+        for value in values:
+            cardVal = "{} {}".format(colour, value)
+            deck.append(cardVal)
+            if value != 0:
+                deck.append(cardVal)
+    return deck
+
+   
+def shuffleDeck(deck):
+    """
+    Shuffles a list of items passed into it
+    Parametre : deck->list
+    Return values : deck->list
+    """
+    random.shuffle(deck)
+    return deck
+
+print(shuffleDeck(builddeck()))
